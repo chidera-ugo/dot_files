cf8dbb0e08098da7517d5ebde0342464f22ece3a
chidera ugo <chiderahugo@gmail.com>
2025-08-21 22:31:49 +0100

    Add spectre keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -55,3 +55,16 @@ map("n", "M", vim.diagnostic.open_float)
 map("n", "[d", vim.diagnostic.goto_prev)
 map("n", "]d", vim.diagnostic.goto_next)
 map("n", "<space>q", vim.diagnostic.setloclist)
+
+map("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
+	desc = "Toggle Spectre",
+})
+map("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+	desc = "Search current word",
+})
+map("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+	desc = "Search current word",
+})
+map("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+	desc = "Search on current file",
+})
